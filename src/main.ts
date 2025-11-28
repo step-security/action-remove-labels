5b8c7bd6ec5e6e64ff6723f9d3fade6e4452255e
amanstep <aman@stepsecurity.io>
2025-11-28 17:31:03 +0530

    feat: Initial release

---
+++ src/main.ts
@@ -0,0 +1,58 @@
+import * as github from '@actions/github';
+import * as core from '@actions/core';
+
+async function run(): Promise<void> {
+  try {
+    const githubToken = core.getInput('github_token');
+
+    const labels = core
+      .getInput('labels')
+      .split('\n')
+      .filter(l => l !== '');
+    const [owner, repo] = core.getInput('repo').split('/');
+    const number =
+      core.getInput('number') === ''
+        ? github.context.issue.number
+        : parseInt(core.getInput('number'));
+
+    if (labels.length === 0) {
+      return;
+    }
+
+    const client = github.getOctokit(githubToken);
+
+    const remaining = [];
+    for (const label of labels) {
+      try {
+        await client.rest.issues.removeLabel({
+          name: label,
+          owner,
+          repo,
+          issue_number: number
+        });
+      } catch (e) {
+        core.warning(`failed to remove label: ${label}: ${e}`);
+        remaining.push(label);
+      }
+    }
+
+    if (remaining.length) {
+      throw new Error(`failed to remove labels: ${remaining}`);
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      core.error(e);
+      if (core.getInput('fail_on_error') === 'true') {
+        core.setFailed(e.message);
+      }
+    } else {
+      const errorMessage = String(e);
+      core.error(errorMessage);
+      if (core.getInput('fail_on_error') === 'true') {
+        core.setFailed(errorMessage);
+      }
+    }
+  }
+}
+
+run();
